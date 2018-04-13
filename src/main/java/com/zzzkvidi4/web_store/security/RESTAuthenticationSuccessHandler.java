0ed95ab5e3220508c832717d9d0f48ae2fed877e
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-04-13 11:22:56 +0300

    Added logging to security system

--- src/main/java/com/zzzkvidi4/web_store/security/RESTAuthenticationSuccessHandler.java
+++ src/main/java/com/zzzkvidi4/web_store/security/RESTAuthenticationSuccessHandler.java
@@ -1,16 +1,19 @@
 package com.zzzkvidi4.web_store.security;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class RESTAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        Logger.getLogger("com.zzzkvidi4.web_store.security").info("Successfully logged in as " + authentication.getName() + "[" + authentication.getAuthorities().toString() + "]");
         clearAuthenticationAttributes(request);
     }
 }
