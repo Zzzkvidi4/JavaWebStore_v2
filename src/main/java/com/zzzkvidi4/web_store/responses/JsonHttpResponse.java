5c0ed1a25dbc7440d267c5a5e4b0d65d103456b8
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-04-14 13:47:28 +0300

    Created system to create, update, delete users from ROLE_USER Created generic response to send all data with one format

---
+++ src/main/java/com/zzzkvidi4/web_store/responses/JsonHttpResponse.java
@@ -0,0 +1,60 @@
+package com.zzzkvidi4.web_store.responses;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonIgnoreType
+public class JsonHttpResponse<T> {
+    private List<String> errors = new ArrayList<>();
+    private T data;
+    private boolean isSuccessful;
+
+    public JsonHttpResponse(){
+        isSuccessful = true;
+    }
+
+    public JsonHttpResponse(T data, boolean isSuccessful){
+        this.data = data;
+        this.isSuccessful = isSuccessful;
+    }
+
+    public void addError(String error){
+        isSuccessful = false;
+        errors.add(error);
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<String> errors) {
+        this.errors = errors;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public boolean isSuccessful() {
+        return isSuccessful;
+    }
+
+    public void setSuccessful(boolean successful) {
+        isSuccessful = successful;
+    }
+
+    public void addErrors(Throwable e){
+        isSuccessful = false;
+        Throwable cur = e;
+        while (cur != null){
+            addError(cur.getMessage());
+            cur = cur.getCause();
+        }
+    }
+}
---
+++ src/main/java/com/zzzkvidi4/web_store/responses/RedirectData.java
@@ -0,0 +1,9 @@
+package com.zzzkvidi4.web_store.responses;
+
+public class RedirectData {
+    private String redirect;
+
+    public RedirectData(String redirect){
+        this.redirect = redirect;
+    }
+}
