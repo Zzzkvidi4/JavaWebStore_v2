e2f6cc4f923bbae525e2ca8484144f778bea5ce0
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-04-20 12:36:00 +0300

    Reorganized orderItemService to be more small and clean

--- src/main/java/com/zzzkvidi4/web_store/services/HibernateOrderItemService.java
+++ src/main/java/com/zzzkvidi4/web_store/services/HibernateOrderItemService.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.LinkedList;
@@ -13,44 +14,64 @@ import java.util.List;
 
 @Service("orderItemService")
 public class HibernateOrderItemService implements OrderItemService {
+    @Resource(name = "productService")
+    private ProductService productService;
+
     @Override
     public List<String> createOrderItem(OrderItem item, String username) {
         List<String> errors = new LinkedList<>();
         try (Session session = DBHelper.getSession()) {
             Transaction transaction = session.beginTransaction();
-            Product product = session.createQuery("from Product where id = :id", Product.class)
-                    .setParameter("id", item.getProductId()).getSingleResult();
-            List<Order> openedOrders = session.createQuery("from Order where status.name like '%OPENED%'", Order.class).list();
-            Order order;
-            if (openedOrders.size() == 0) {
-                Status status = session.createQuery("from Status where name like '%OPENED%'", Status.class).getSingleResult();
-                User user = session.createQuery("from User where login = :username", User.class)
-                        .setParameter("username", username).getSingleResult();
-                order = new Order();
-                order.setDate(Timestamp.valueOf(LocalDateTime.now()));
-                order.setUserId(user.getUserId());
-                order.setStatusId(status.getStatusId());
-                order.setPrice(0);
-                session.save(order);
-            } else {
-                order = openedOrders.get(0);
+            Product product = productService.findProductById(session, item.getProductId());
+            if (product == null) {
+                errors.add("No product with such id!");
+                return errors;
             }
+            Order order = findOpenedOrder(session, username);
             item.setOrderId(order.getOrderId());
-            int productCount = product.getCount();
-            int itemCount = item.getCount();
-            if (productCount >= itemCount) {
-                product.setCount(productCount - itemCount);
-                int prevPrice = order.getPrice();
-                order.setPrice(prevPrice + itemCount * product.getPrice());
-                session.save(item);
-                session.save(order);
-                session.save(product);
+
+            if (product.getCount() >= item.getCount()) {
+                setUpOrderItem(session, product, order, item);
                 transaction.commit();
             } else {
                 errors.add("Not enough product on storage " + product.getName());
                 transaction.rollback();
             }
+
             return errors;
         }
+    }
+
+    private Order createOrder(Session session, String username){
+        Status status = session.createQuery("from Status where name like '%OPENED%'", Status.class).getSingleResult();
+        User user = session
+                .createQuery("from User where login = :username", User.class)
+                .setParameter("username", username)
+                .getSingleResult();
+        Order order = new Order();
+        order.setDate(Timestamp.valueOf(LocalDateTime.now()));
+        order.setUserId(user.getUserId());
+        order.setStatusId(status.getStatusId());
+        order.setPrice(0);
+        session.save(order);
+        return order;
+    }
+
+    private Order findOpenedOrder(Session session, String username){
+        List<Order> openedOrders = session.createQuery("from Order where status.name like '%OPENED%'", Order.class).list();
+        if (openedOrders.size() == 0) {
+            return createOrder(session, username);
+        } else {
+            return openedOrders.get(0);
+        }
+    }
+
+    private void setUpOrderItem(Session session, Product product, Order order, OrderItem item){
+        product.setCount(product.getCount() - item.getCount());
+        int prevPrice = order.getPrice();
+        order.setPrice(prevPrice + item.getCount() * product.getPrice());
+        session.save(item);
+        session.save(order);
+        session.save(product);
     }
 }
