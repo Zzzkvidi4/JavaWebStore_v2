ac50960d6bb32ab16276dd67931313f80d21b0d1
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-04-22 08:44:14 +0300

    Added cors configurations to allow cross origin requests. Tested at login page

--- src/main/java/com/zzzkvidi4/web_store/security/SecurityAdapter.java
+++ src/main/java/com/zzzkvidi4/web_store/security/SecurityAdapter.java
@@ -1,13 +1,18 @@
 package com.zzzkvidi4.web_store.security;
 
 import com.zzzkvidi4.web_store.services.UserDetailsService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +27,7 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http.authorizeRequests().antMatchers("/greeting/**", "/register/**", "/product_types/**").permitAll();
         http.authorizeRequests().antMatchers("/users/**").hasAnyRole("USER");
         http.exceptionHandling().authenticationEntryPoint(new RESTAuthenticationEntryPoint());
@@ -29,5 +35,16 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
         http.formLogin().failureHandler(new RESTAuthenticationFailureHandler());
         http.logout();
         http.csrf().disable();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
