c56bc094287d8cf416d3793b13cd1c6b3842d86f
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-04-24 20:41:26 +0300

    Created product-list component

--- src/main/web-store/src/app/product-list/product-list.component.ts
+++ src/main/web-store/src/app/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {ProductTypeService} from "../services/product-type-service/product-type.service";
+import {ActivatedRoute} from "@angular/router";
+import {Product} from "../models/product";
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +10,24 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = null;
+
+  constructor(
+    private productTypeService: ProductTypeService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.uploadProducts();
   }
 
+  uploadProducts(){
+    let productTypeId = +this.route.snapshot.paramMap.get("productTypeId");
+    this.productTypeService.getProductsByProductTypeId(productTypeId).subscribe(resp => {
+      console.log(resp);
+      if (resp.status == 200) {
+        this.products = resp.body.data;
+      }
+    })
+  }
 }
