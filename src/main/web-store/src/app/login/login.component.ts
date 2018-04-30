20ecbf95015cfc0cef37f840f62f77056f390d13
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-04-30 14:25:39 +0300

    Added redirect in login component after successful login

--- src/main/web-store/src/app/login/login.component.ts
+++ src/main/web-store/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../services/user-service/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -10,13 +11,21 @@ export class LoginComponent implements OnInit {
   username: string;
   password: string;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
   login(){
-    this.userService.login(this.username, this.password).subscribe(resp => console.log(resp));
+    this.userService.login(this.username, this.password).subscribe(
+      resp => {
+        console.log(resp);
+        this.router.navigate(["/product_types"]);
+      }
+    );
   }
 
 }
