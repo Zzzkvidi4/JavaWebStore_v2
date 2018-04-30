a2d6199a27487ce4f05e40f35ec99c01898c762b
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-04-30 14:28:55 +0300

    Added possibility to buy product

--- src/main/web-store/src/app/product/product.component.ts
+++ src/main/web-store/src/app/product/product.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {ProductService} from "../services/product-service/product.service";
 import {OrderService} from "../services/order-service/order.service";
 import {Product} from "../models/product";
+import {OrderItem} from "../models/order-item";
 
 @Component({
   selector: 'app-product',
@@ -13,20 +14,22 @@ export class ProductComponent implements OnInit {
 
   product: Product = null;
   count: number = 1;
+  productId: number;
 
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private router: Router
   ) { }
 
   ngOnInit() {
+    this.productId = +this.route.snapshot.paramMap.get("productId");
     this.uploadProduct();
   }
 
   uploadProduct(){
-    let productId = +this.route.snapshot.paramMap.get("productId");
-    this.productService.getProductById(productId).subscribe(resp => {
+    this.productService.getProductById(this.productId).subscribe(resp => {
         console.log(resp);
         if (resp.status == 200) {
           this.product = resp.body.data;
@@ -36,6 +39,16 @@ export class ProductComponent implements OnInit {
   }
 
   addToOrder(){
-
+    let item = new OrderItem();
+    item.productId = this.productId;
+    item.count = this.count;
+    let tmp = this.orderService.createOrderItem(item);
+    tmp.subscribe(resp => {
+      this.router.navigate(["/product_types"]);
+    }, error => {
+      if (error.status == 401) {
+        this.router.navigate(["/login"]);
+      }
+    });
   }
 }
