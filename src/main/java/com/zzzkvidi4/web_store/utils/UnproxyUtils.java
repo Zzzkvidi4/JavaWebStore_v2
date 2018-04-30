014acb2c4fe2a8caa07957cc296d7234ff3640bb
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-04-30 14:19:37 +0300

    Implemented another one method in UnproxyUtils to detach order from db

--- src/main/java/com/zzzkvidi4/web_store/utils/UnproxyUtils.java
+++ src/main/java/com/zzzkvidi4/web_store/utils/UnproxyUtils.java
@@ -1,5 +1,7 @@
 package com.zzzkvidi4.web_store.utils;
 
+import com.zzzkvidi4.web_store.models.Order;
+import com.zzzkvidi4.web_store.models.OrderItem;
 import com.zzzkvidi4.web_store.models.Product;
 import com.zzzkvidi4.web_store.models.ProductType;
 
@@ -19,6 +21,25 @@ public class UnproxyUtils {
         tmp.setPrice(product.getPrice());
         tmp.setProductTypeId(product.getProductTypeId());
         tmp.setCountryId(product.getCountryId());
+        return tmp;
+    }
+
+    public static Order unproxy(Order order, boolean withItems){
+        Order tmp = new Order();
+        tmp.setDate(order.getDate());
+        tmp.setOrderId(order.getOrderId());
+        tmp.setStatus(order.getStatus());
+        if (withItems) {
+            order.getItems().forEach(item -> {
+                OrderItem bufItem = new OrderItem();
+                bufItem.setProduct(unproxy(item.getProduct()));
+                bufItem.setOrderItemId(item.getOrderItemId());
+                bufItem.setProductId(item.getProductId());
+                bufItem.setCount(item.getCount());
+                tmp.getItems().add(bufItem);
+            });
+        }
+        tmp.setPrice(order.getPrice());
         return tmp;
     }
 }
